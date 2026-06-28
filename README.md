ef5f39a02b3eea3276d47025ec3556bf713605d1
Ametha Jayawardena 🐺 <amethacodes@gmail.com>
2026-06-28 20:32:26 +0530

    merge branch 'ametha/create_expense_records_store' to 'main'

---
+++ README.md
@@ -0,0 +1,5 @@
+**Expense Tracking For Noob Devs**  
+
+I'll record whatever bits I might forget in the future for now; proper documentation will be done when I think its ready :)  
+
+* Both DDL annotations (shape the DB tables) and Java validation annotations (validates Java code)  are needed
---
+++ src/main/java/com/example/demo/model/Expense.java
@@ -0,0 +1,99 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.persistence.Id;
+
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Amount is required!")
+    @Positive(message = "Amount must be greater than 0")
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @NotNull(message = "Date of transaction is required!")
+    @Column(name = "date_of_transaction", nullable = false)
+    private LocalDate dateOfTransaction;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDate createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDate updatedAt;
+
+    @NotBlank(message = "Description cannot be empty!")
+    @Column(name = "description")
+    private String description;
+
+    public Expense(BigDecimal amount, LocalDate dateOfTransaction, LocalDate createdAt, LocalDate updatedAt, String description) {
+        this.amount = amount;
+        this.dateOfTransaction = dateOfTransaction;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.description = description;
+    }
+
+    public Expense() {}
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public BigDecimal getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDateOfTransaction() {
+        return this.dateOfTransaction;
+    }
+
+    public void setDateOfTransaction(LocalDate dateOfTransaction) {
+        this.dateOfTransaction = dateOfTransaction;
+    }
+
+    public LocalDate getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDate getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDate updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
