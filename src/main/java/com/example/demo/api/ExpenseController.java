8d6c83b89b3138158f055ff236e431159ed40a4d
Ametha Jayawardena 🐺 <amethacodes@gmail.com>
2026-06-29 14:35:08 +0530

    merge branch 'ametha/implement_endpoints_for_all_basic_crud_in_expenses' to 'main'

--- src/main/java/com/example/demo/api/ExpenseController.java
+++ src/main/java/com/example/demo/api/ExpenseController.java
@@ -1,14 +1,23 @@
 package com.example.demo.api;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Expense;
 import com.example.demo.service.ExpenseService;
 
-@RequestMapping("api/v1/expense")
+@RequestMapping("api/v1/expense-tracking")
 @RestController
 public class ExpenseController {
     private final ExpenseService expenseService;
@@ -20,5 +29,32 @@ public class ExpenseController {
     @PostMapping
     public void addExpense(@RequestBody Expense expense) {
         expenseService.createExpense(expense);
+    }
+
+    @PutMapping("/update-record/{id}")
+    public void updateExpenseRecord(
+        @PathVariable("id") Long id,
+        @RequestParam(name = "description", required = false) String description,
+        @RequestParam(name = "amount", required = false) BigDecimal amount,
+        @RequestParam(name = "dateOfTransaction", required = false) LocalDate date
+    ) {
+        expenseService.updateExpense(id, description, amount, date);
+    }
+
+    @GetMapping("/search")
+    public List<Expense> getExpenseRecords(
+        @RequestParam(name = "description", required = false) String description,
+        @RequestParam(name = "date", required = false) LocalDate date) {
+    
+        return expenseService.searchExpenses(description, date);
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteExpenseRecords(
+        @RequestParam("deletionIds") List<Long> ids) {
+            if (ids.size() == 1)
+                return expenseService.deleteById(ids.get(0));
+            else
+                return expenseService.deleteById(ids);
     }
 }
--- src/main/java/com/example/demo/service/ExpenseService.java
+++ src/main/java/com/example/demo/service/ExpenseService.java
@@ -1,21 +1,68 @@
 package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.ExpenseRepository;
 import com.example.demo.model.Expense;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class ExpenseService {
     private final ExpenseRepository expenseRepository;
 
-    @Autowired
     public ExpenseService(ExpenseRepository expenseRepository) {
         this.expenseRepository = expenseRepository;
     }
 
     public Expense createExpense(Expense expense) {
         return expenseRepository.save(expense);
+    }
+
+    public void updateExpense(Long id, String description, BigDecimal amount, LocalDate date) {
+        Optional<Expense> optionalExpense = expenseRepository.findById(id);
+
+        if (!optionalExpense.isPresent()) return;
+        Expense existingExpense = optionalExpense.get();
+
+        if (description != null && !description.isBlank())
+            existingExpense.setDescription(description);
+        if (amount != null)
+            existingExpense.setAmount(amount);
+        if (date != null)
+            existingExpense.setDateOfTransaction(date);
+
+        expenseRepository.save(existingExpense);
+    }
+
+    public List<Expense> searchExpenses(String mustIncludeInDescription, LocalDate date) {
+        if (mustIncludeInDescription != null && date != null)
+            return expenseRepository.findByDescriptionContainingIgnoreCaseAndDateOfTransaction(mustIncludeInDescription, date);
+        else if (mustIncludeInDescription != null)
+            return expenseRepository.findByDescriptionContainingIgnoreCase(mustIncludeInDescription);
+        else if (date != null)
+            return expenseRepository.findByDateOfTransaction(date);
+        else
+            return expenseRepository.findAll();
+    }
+
+    @Transactional
+    public String deleteById(Long id) {
+        if (!expenseRepository.existsById(id))
+            return "No expense records with the requested ID " + id + " exists.";
+        expenseRepository.deleteById(id);
+        
+        return "Successfully deleted the expense record with ID " + id; 
+    }
+
+    @Transactional
+    public String deleteById(List<Long> ids) {
+        expenseRepository.deleteAllById(ids);
+        return "Successfully deleted the expense records with ID's " + ids;
     }
 }
